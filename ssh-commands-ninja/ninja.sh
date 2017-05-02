fc77ad6fd58c3ddfd3d11f7bd3729824ba3476f6
Bruno Maciel massaini <bruno.massaini@ip-172-17-21-54.eu-west-1.compute.internal>
2017-05-02 17:01:59 +0200

    last changes

--- ssh-commands-ninja/ninja.sh
+++ ssh-commands-ninja/ninja.sh
@@ -4,7 +4,6 @@ echo "Loading Configuration"
 IP_SERVER_LIST="("`jq -r '.IP_LIST' config.json | sed "s/\"/'/g" | sed 's/\,//g' | sed 's/\[//g' | sed 's/\]//g'`")"
 SSHUSER=`jq -r '.SSH_USER' config.json`
 SSHKEY=`jq -r '.SSH_KEY' config.json`
-COMMANDS=commands.sh
 echo "- Servers IPs:" $IP_SERVER_LIST
 
 declare -a IP_SERVER_LIST_ARRAY=$IP_SERVER_LIST;
@@ -14,9 +13,12 @@ for i in ${!IP_SERVER_LIST_ARRAY[@]} ; do
     echo "Running Commands"
     echo "- Server" `expr $i + 1`":" ${IP_SERVER_LIST_ARRAY[$i]}
 
-    ssh -T -i $SSHKEY $SSHUSER@${IP_SERVER_LIST_ARRAY[$i]} $COMMANDS
+    echo "-- Copying bash Script to Host"
+    scp -i $SSHKEY commands.sh $SSHUSER@${IP_SERVER_LIST_ARRAY[$i]}:/home/$SSHUSER &> /dev/null
+
+    echo "-- Running Commands"
+    ssh -o "StrictHostKeyChecking no" -T -i $SSHKEY $SSHUSER@${IP_SERVER_LIST_ARRAY[$i]} "chmod +x /home/$SSHUSER/commands.sh && /home/$SSHUSER/commands.sh && rm commands.sh"
     
-    exit
 done
 
 echo "All commands finished"
